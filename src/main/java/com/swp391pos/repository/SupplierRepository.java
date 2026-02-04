6256bed22c6a2424490fc9f7b6e21fd61dea525c
KimOPT <nguyenphongkien1512k@gmail.com>
2026-02-04 21:45:23 +0700

    modify SupplierRepository

--- src/main/java/com/swp391pos/repository/SupplierRepository.java
+++ src/main/java/com/swp391pos/repository/SupplierRepository.java
@@ -1,17 +1,16 @@
 package com.swp391pos.repository;
 
 import com.swp391pos.entity.Supplier;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
     @Query("SELECT s.supplierId, s.supplierName, s.contactName, s.email, " +
-            "SUM(sid.actualQuantity * sid.unitPriceAtPurchase) " +
+            "SUM(sid.receivedQuantity * sid.unitCost) " +
             "FROM Supplier s " +
             "LEFT JOIN StockIn si ON s.supplierId = si.supplier.supplierId " +
             "LEFT JOIN StockInDetail sid ON si.stockInId = sid.stockIn.stockInId " +
-            "GROUP BY s.supplierId")
+            "GROUP BY s.supplierId,s.supplierName, s.contactName, s.email")
     List<Object[]> findAllSuppliersWithTotalValue();
 }
