e155ce4c2254ad98c39cc4e6a2f32db5cb9a12e9
KimOPT <nguyenphongkien1512k@gmail.com>
2026-02-24 23:48:52 +0700

    find stockout, audit by transactionstatus

--- src/main/java/com/swp391pos/repository/AuditSessionRepository.java
+++ src/main/java/com/swp391pos/repository/AuditSessionRepository.java
@@ -1,7 +1,15 @@
 package com.swp391pos.repository;
 
 import com.swp391pos.entity.AuditSession;
+import com.swp391pos.entity.StockIn;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AuditSessionRepository extends JpaRepository<AuditSession, Integer> {
+    @Query("SELECT a FROM AuditSession a WHERE a.status.transactionStatusId = :statusId")
+    List<AuditSession> findByStatusId(@Param("statusId") Integer statusId);
+    StockIn findByAuditId(int auditId);
 }
