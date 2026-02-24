a1a72ad19e446a1fdf6dcfb28ff601efb1a9dd79
buiqhuy05 <buiquanghuy1112005@gmail.com>
2026-02-25 01:05:00 +0700

    Fix: bug sql in PromotionRepo

--- src/main/java/com/swp391pos/repository/PromotionRepository.java
+++ src/main/java/com/swp391pos/repository/PromotionRepository.java
@@ -15,7 +15,7 @@ import java.util.List;
 public interface PromotionRepository extends JpaRepository<Promotion, Integer> {
 
     @Query("SELECT p FROM Promotion p WHERE p.status != 'DELETED' " +
-            "(:keyword IS NULL OR p.promoName LIKE %:keyword% OR CAST(p.promotionId AS string) LIKE %:keyword%) " +
+            "AND(:keyword IS NULL OR p.promoName LIKE %:keyword% OR CAST(p.promotionId AS string) LIKE %:keyword%) " +
             "AND (:status IS NULL OR p.status = :status) " +
             "AND (:fromDate IS NULL OR p.startDate >= :fromDate) " +
             "AND (:toDate IS NULL OR p.startDate <= :toDate)") // Lọc trong khoảng ngày bắt đầu
