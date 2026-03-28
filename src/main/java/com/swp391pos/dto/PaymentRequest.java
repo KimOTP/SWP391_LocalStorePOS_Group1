65433ddd3e45d0493fe35c107262df0551d0fa55
Vieetj-Anh <hovietanh2812@gmail.com>
2026-03-28 22:21:48 +0700

    remove unuse component

--- src/main/java/com/swp391pos/dto/PaymentRequest.java
+++ src/main/java/com/swp391pos/dto/PaymentRequest.java
@@ -8,9 +8,6 @@ import java.math.BigDecimal;
 public class PaymentRequest {
     private Long orderId;
     private BigDecimal amount;
-
-    // [FIX] Them gatewayOrderCode va paymentSessionId
-    // de gateway nhan du thong tin, khong dung request goc tu POS
     private String gatewayOrderCode;
     private String paymentSessionId;
 
